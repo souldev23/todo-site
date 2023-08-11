d98be08fe1b892386aa41143b3ab6b54ce7b3682
Souldev <soul23k@hotmail.com>
2023-08-10 20:24:19 -0600

    First commit

---
+++ src/todos/models/todo.js
@@ -0,0 +1,13 @@
+import { td } from "../models/todo";
+
+cons
+
+const state = {
+    todos: [
+        new td('Piedra del alma'),
+        new td('Piedra de la mente'),
+        new td('Piedra del poder'),
+        new td('Piedra del tiempo')
+    ],
+    filter: 'all'
+}
